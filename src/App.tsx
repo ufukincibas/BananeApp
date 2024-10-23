c35c3e35c25a42a84ab9f54598e9902bf592bc5e
ufukincibas <ufukincibas@hotmail.com>
2024-10-23 23:15:10 +0300

    logout Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,63 @@
-import React from "react";
-import {Text , View } from "react-native"
+import React, { useEffect, useState } from "react";
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import Login from "./pages/auth/Login/Login"
 import Sign from "./pages/auth/Sign/Sign";
 import FlashMessage from "react-native-flash-message";
 import Messages from "./pages/Messages/Messages";
 
+import auth from "@react-native-firebase/auth"
+import Icon from "react-native-vector-icons/MaterialCommunityIcons"
+
 
 const Stack = createNativeStackNavigator()
 
 function App(){
+
+    const [userSession , setUserSession] = useState();
+
+    useEffect(() => { //acıldıgı anda
+      auth().onAuthStateChanged((user) => {
+        setUserSession(!!user) //dolu geliyosa true bossa false set eder
+      })
+
+    } , [])
+
   const AuthStack = () => {
     return (
-      <Stack.Navigator>
-       <Stack.Screen name="MessagesPage" component={Messages}/>
-      <Stack.Screen name="LoginPage" component={Login}/>
-      <Stack.Screen name="SignPage" component={Sign}/>
+      <Stack.Navigator screenOptions={{headerShown:false}}>
+      
+      <Stack.Screen name="LoginPage" component={Login} />
+      <Stack.Screen name="SignPage" component={Sign} />
     </Stack.Navigator>
  )
   }
   return(
    <NavigationContainer>
-    <Stack.Navigator screenOptions={
-      {headerShown:false}
-    }>
      
-      <Stack.Screen name="AuthStack" component={AuthStack}/>
+    {
+      !userSession ? (
+      <AuthStack/>
+    ):(
+      <Stack.Navigator screenOptions={{headerShown:false}} >
+      <Stack.Screen name="MessagesPage" component={Messages}
+      options={{
+        headerShown:true,
+        title: "Dertler",
+        headerTintColor : "#4CAF50",
+        headerRight: () => (<Icon name="logout" size={30} color="#4CAF50"
+        onPress={() => auth().signOut()}
+        />)
+      }}/>
+      </Stack.Navigator>
+    )}
+   
+     
+    
  
-    </Stack.Navigator>
+   
     <FlashMessage position="top" />
    </NavigationContainer>
   )
